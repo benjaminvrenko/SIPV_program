579717f05769ed8e54fe4cc40b4ca507f966f69c
pumpabeno <39494728+pumpabeno@users.noreply.github.com>
2018-05-21 18:34:21 +0200

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.naslovBox = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.avtorBox = new System.Windows.Forms.TextBox();
+            this.kljucnebesedeBox = new System.Windows.Forms.TextBox();
+            this.letoizidaBox = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.toolStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(904, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(67, 22);
+            this.toolStripDropDownButton1.Text = "Datoteka";
+            this.toolStripDropDownButton1.ToolTipText = "Datoteka";
+            this.toolStripDropDownButton1.Click += new System.EventHandler(this.toolStripDropDownButton1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.comboBox4);
+            this.panel1.Controls.Add(this.comboBox3);
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.checkedListBox1);
+            this.panel1.Controls.Add(this.letoizidaBox);
+            this.panel1.Controls.Add(this.kljucnebesedeBox);
+            this.panel1.Controls.Add(this.avtorBox);
+            this.panel1.Controls.Add(this.searchButton);
+            this.panel1.Controls.Add(this.naslovBox);
+            this.panel1.Location = new System.Drawing.Point(13, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(296, 552);
+            this.panel1.TabIndex = 1;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(4, 199);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "Išči";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // naslovBox
+            // 
+            this.naslovBox.Location = new System.Drawing.Point(4, 3);
+            this.naslovBox.Name = "naslovBox";
+            this.naslovBox.Size = new System.Drawing.Size(216, 20);
+            this.naslovBox.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(422, 29);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(470, 552);
+            this.panel2.TabIndex = 2;
+            // 
+            // avtorBox
+            // 
+            this.avtorBox.Location = new System.Drawing.Point(4, 29);
+            this.avtorBox.Name = "avtorBox";
+            this.avtorBox.Size = new System.Drawing.Size(216, 20);
+            this.avtorBox.TabIndex = 4;
+            // 
+            // kljucnebesedeBox
+            // 
+            this.kljucnebesedeBox.Location = new System.Drawing.Point(4, 55);
+            this.kljucnebesedeBox.Name = "kljucnebesedeBox";
+            this.kljucnebesedeBox.Size = new System.Drawing.Size(216, 20);
+            this.kljucnebesedeBox.TabIndex = 6;
+            // 
+            // letoizidaBox
+            // 
+            this.letoizidaBox.Location = new System.Drawing.Point(4, 81);
+            this.letoizidaBox.Name = "letoizidaBox";
+            this.letoizidaBox.Size = new System.Drawing.Size(216, 20);
+            this.letoizidaBox.TabIndex = 8;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Naslov",
+            "Avtor",
+            "Opis",
+            "Ključne besede",
+            "Čisto besedilo",
+            "Leto izida"});
+            this.checkedListBox1.Location = new System.Drawing.Point(80, 422);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(107, 94);
+            this.checkedListBox1.TabIndex = 2;
+            this.checkedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "IN",
+            "ALI"});
+            this.comboBox1.Location = new System.Drawing.Point(226, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(60, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "IN",
+            "ALI"});
+            this.comboBox2.Location = new System.Drawing.Point(227, 28);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(60, 21);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "IN",
+            "ALI"});
+            this.comboBox3.Location = new System.Drawing.Point(226, 54);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(60, 21);
+            this.comboBox3.TabIndex = 11;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "IN",
+            "ALI"});
+            this.comboBox4.Location = new System.Drawing.Point(227, 80);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(60, 21);
+            this.comboBox4.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 593);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TextBox naslovBox;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TextBox letoizidaBox;
+        private System.Windows.Forms.TextBox kljucnebesedeBox;
+        private System.Windows.Forms.TextBox avtorBox;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        string naslov, avtor, opis, kljucnebesede, cistobesedilo, letoizida, val1, val2, val3, val4;
+
+        int stevecVnosov = 0;
+        
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            string naslovSearch, avtorSearch, kljucnebesedeSearch, letoizidaSearch = null;
+            
+            naslov = naslovBox.Text;
+            avtor = avtorBox.Text;
+            kljucnebesede = kljucnebesedeBox.Text;
+            letoizida = letoizidaBox.Text;
+
+            System.Net.WebClient client = new System.Net.WebClient();
+
+
+            if(naslovBox.Text!= "")
+            {
+                stevecVnosov++;
+
+                string op = null;
+                string naslovBesede = naslov.Replace(" ", "+");
+
+
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    op = "and";
+                }
+                else
+                {
+                    op = "or";
+                }
+
+
+                string zlepljenString = "op" + stevecVnosov + "=" + op + "&val" + stevecVnosov + "=" + naslovBesede + "&col" + stevecVnosov + "=naslov&";
+
+                //op1=and&val1=nekaj&col1=naslov
+
+                naslovSearch = client.DownloadString("https://dk.um.si/ajax.php?cmd=getAdvancedSearch&source=dk&workType=0&language=0&" + zlepljenString +  "page=1");
+
+            }
+            if (avtorBox.Text != "")
+            {
+                stevecVnosov++;
+
+                string op = null;
+                string avtorBesede = avtor.Replace(" ", "+");
+
+
+                if (comboBox2.SelectedIndex == 0)
+                {
+                    op = "and";
+                }
+                else
+                {
+                    op = "or";
+                }
+
+
+                string zlepljenString = "op" + stevecVnosov + "=" + op + "&val" + stevecVnosov + "=" + avtorBesede + "&col" + stevecVnosov + "=avtor&";
+
+                //op1=and&val1=nekaj&col1=naslov
+
+                avtorSearch = client.DownloadString("https://dk.um.si/ajax.php?cmd=getAdvancedSearch&source=dk&workType=0&language=0&" + zlepljenString + "page=1");
+            }
+            if (kljucnebesedeBox.Text != "")
+            {
+                stevecVnosov++;
+
+                string op = null;
+                string kljucnebesedeBesede = kljucnebesede.Replace(" ", "+");
+
+
+                if (comboBox3.SelectedIndex == 0)
+                {
+                    op = "and";
+                }
+                else
+                {
+                    op = "or";
+                }
+
+
+                string zlepljenString = "op" + stevecVnosov + "=" + op + "&val" + stevecVnosov + "=" + kljucnebesedeBesede + "&col" + stevecVnosov + "=kljucneBesede&";
+
+                //op1=and&val1=nekaj&col1=naslov
+
+                kljucnebesedeSearch = client.DownloadString("https://dk.um.si/ajax.php?cmd=getAdvancedSearch&source=dk&workType=0&language=0&" + zlepljenString + "page=1");
+            }
+            if (letoizidaBox.Text != "")
+            {
+                stevecVnosov++;
+
+                string op = null;
+                string letoizidaBesede = letoizida.Replace(" ", "+");
+
+
+                if (comboBox4.SelectedIndex == 0)
+                {
+                    op = "and";
+                }
+                else
+                {
+                    op = "or";
+                }
+
+
+                string zlepljenString = "op" + stevecVnosov + "=" + op + "&val" + stevecVnosov + "=" + letoizidaBesede + "&col" + stevecVnosov + "=letoIzida&";
+
+                //op1=and&val1=nekaj&col1=naslov
+
+                letoizidaSearch = client.DownloadString("https://dk.um.si/ajax.php?cmd=getAdvancedSearch&source=dk&workType=0&language=0&" + zlepljenString + "page=1");
+            }
+
+
+
+            if (naslovBox.Text != "")
+            {
+
+                string[] naslovBesede = naslov.Split(null);
+                string zlepljenString = null;
+
+                for (int i = 1; i <= naslovBesede.Length; i++)
+                {
+                    zlepljenString += "val" + i + "=" + naslovBesede[i - 1] + "&";
+                }
+
+                naslovSearch = client.DownloadString("https://dk.um.si/ajax.php?cmd=getAdvancedSearch&source=dk&workType=0&language=0&op1=and&" + zlepljenString + "col1=naslov&page=1");
+            }
+
+
+
+        }
+
+        private void opisBox_TextChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("bla");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripDropDownButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 0;
+            comboBox4.SelectedIndex = 0;
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+
+        }
+    }
+}
